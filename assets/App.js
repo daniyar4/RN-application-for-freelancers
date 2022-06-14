6cdfa8c509a81937411a6cfc30e22253489378d3
daniyar4 <daniiarkhakimov@gmail.com>
2022-06-14 18:02:13 +0300

    second commit

--- assets/App.js
+++ assets/App.js
@@ -5,47 +5,36 @@ import ReduxThunk from 'redux-thunk';
 import * as Font from 'expo-font';
 import AppLoading from 'expo-app-loading';
 import {LogBox} from 'react-native';
-import {Asset} from 'expo-asset';
 
-import images from './assets/images.js'
 import reducer from './redux/reducer';
 import MainNavigator from './Components/Navigator.js';
+import {Asset} from 'expo-asset';
 
-
+import 'react-native-gesture-handler';
 
 LogBox.ignoreLogs(['Setting a timer'])
 
 const store = createStore(reducer, applyMiddleware(ReduxThunk));
 
-const fetchFonts = () => {
-    return Font.loadAsync({
-        'roboto': require('./assets/Roboto-Regular.ttf'),
-    })
-}
-
-const fetchImages = async () => {
-    const cacheImages = images.map(image => {
-        return Asset.fromModule(image).downloadAsync()
-    })
-
-    return Promise.all(cacheImages)
-}
-
 async function loadResourcesAsync() {
-    const fontAssets = fetchFonts()
-    const imageAssets = fetchImages()
-
-    await Promise.all([fontAssets, imageAssets])
+    await Promise.all([
+        Asset.loadAsync([
+            require('./assets/menu.png'),
+        ]),
+        Font.loadAsync({
+            'roboto': require('./assets/Roboto-Regular.ttf'),
+        }),
+    ]);
 }
 
 export default function App() {
-    const [assetsLoaded, setAssetsLoaded] = useState(false);
+    const [fontsLoaded, setFontsLoaded] = useState(false);
 
-    if (!assetsLoaded) {
+    if (!fontsLoaded) {
         return (
             <AppLoading
                 startAsync={loadResourcesAsync}
-                onFinish={()=>setAssetsLoaded(true)}
+                onFinish={()=>setFontsLoaded(true)}
                 onError={console.warn}
             />
         );
